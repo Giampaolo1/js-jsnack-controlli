4d95f9fdba14ae2667980ad918159490d877bab8
Giampaolo1 <giampaolo.r@outlook.com>
2019-10-26 15:00:58 +0200

    Need a controllo

--- Jsnack5/script/main.js
+++ Jsnack5/script/main.js
@@ -8,13 +8,23 @@ var array = [];
 //  Chiedi per 6 volte all’utente di inserire un numero
 for (var i = 1; i <= 6; i++) {
   var numero = parseInt(prompt("Inserisci un numero"));
+
+  var numinseriti = document.getElementById('numinseriti').innerHTML;
+  document.getElementById('numinseriti').innerHTML = numinseriti + "<li>" + numero + "</li>";
+
     if (numero % 2 == 0) {
     } else {
       array.push(numero)
     }
 }
 
-document.getElementById("numinseriti").innerHTML = "Num Inseriti: " + numero;
+var numinseriti = document.getElementById('numinseriti').innerHTML;
+document.getElementById('numinseriti').innerHTML = numinseriti + "<li>" + numero + "</li>";
+
+
+// document.getElementById("numinseriti").innerHTML = "Num Inseriti: " + numero;
 
 console.log(array);
 document.getElementById("output").innerHTML = "Num Dispari: " + array;
+
+// IDEA: devo aggiungere un controllo sui doppioni
